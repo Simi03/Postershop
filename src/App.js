ab0f4bf8f463691e590a6cde25db5c7affc7d35a
Simi03 <57719083+Simi03@users.noreply.github.com>
2020-12-16 14:19:35 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { react, useState } from 'react';
 import { Container, Button, Toolbar, IconButton, AppBar } from '@material-ui/core';
@@ -9,15 +8,14 @@ import AddIcon from '@material-ui/icons/Add';
 
 export default function App() {
   // useState = rendender lifetime
-  const [x, setX] = useState(0);
-  const [list, setList] = useState(["1", "2"]);
-  const [products, setProducts] = useState([Products]);
-  const stuff = "yikes";
-  var sht;
 
-  function addToList() {
-    setList(() => [...list, stuff]);
-  } 
+  const [x, setX] = useState(0);
+  const [list, setList] = useState([]);
+  //const [products, setProducts] = useState([p]);
+
+ // function addToList() {
+   // setList(() => [...list, stuff]);
+  //} 
 
   return (
     <Container maxWidth="sm">
@@ -25,23 +23,14 @@ export default function App() {
         <Toolbar>
           <div id="menu-items">
             <h1><span>🦝</span> Postershop</h1>
-            {x}
             <IconButton edge="end" onClick={() => setX(x + 1)}>
               <ShoppingCartIcon />
             </IconButton>
-            <IconButton aria-label="add" onClick={() => addToList()}>
-              <AddIcon />
-            </IconButton>
-            <ul>
-              <li>
-                {list.map((text) => (<h1>{text} </h1>))}
-              </li>
-            </ul>
+            
           </div>
         </Toolbar>
       </AppBar>
       <div>
-      {products}
       </div>
 
     </Container >
